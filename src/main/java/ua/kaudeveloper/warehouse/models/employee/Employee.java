b1a82982d3ac1402104a43d214dc27bc7f2e5a69
kau-dev <kreminsky.development.test@gmail.com>
2022-06-11 10:07:46 +0300

    g s

--- src/main/java/ua/kaudeveloper/warehouse/models/employee/Employee.java
+++ src/main/java/ua/kaudeveloper/warehouse/models/employee/Employee.java
@@ -18,20 +18,60 @@ public class Employee {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-@Getter
-@Setter
- private Long id;
-@Getter
-@Setter
- private String name;
-@Getter
-@Setter
- private String position;
-@Getter
-@Setter
- private int age;
-@Getter
-@Setter
-@ManyToOne(fetch = FetchType.LAZY,cascade=CascadeType.ALL)
- private Storage storage;
+    @Getter
+    @Setter
+    private Long id;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private String position;
+    @Getter
+    @Setter
+    private int age;
+    @Getter
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Storage storage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
+    public void setStorage(Storage storage) {
+        this.storage = storage;
+    }
 }
